8a18cdf2785b9bf6562eba0e5ef607248517241f
vamos91 <acardnicolas@hotmail.com>
2022-10-04 15:57:21 +0200

    test

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.json({response: 'ok, V2'})
-})
+const indexRouter = require('./routes/index')
+
+app.use('/', indexRouter)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
