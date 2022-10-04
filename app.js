693aa7dd45780918f1c7450d41f8ab3b0ea62568
vamos91 <acardnicolas@hotmail.com>
2022-10-04 15:33:54 +0200

    test2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.json({response: 'ok'})
+  res.json({response: 'ok, V2'})
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
